0ece9bed7a09e5903484a77c8747071218206788
Prashant Shukla <prashantshukla@qainfotech.com>
2020-04-27 08:02:53 +0530

    Running tests on chrome

--- src/test/java/com/qait/cucumber/stepdefinitions.java
+++ src/test/java/com/qait/cucumber/stepdefinitions.java
@@ -1,9 +1,15 @@
 package com.qait.cucumber;
 
+import java.io.File;
+
 import com.qait.automation.TestSessionInitiator;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -51,7 +57,14 @@ public class stepdefinitions {
     }
 
     @After()
-    public void closeBrowser() {
+    public void closeBrowser(Scenario s) {
+        if (s.isFailed())
+            try {
+                byte[] screenshot = ((TakesScreenshot) test.getDriver()).getScreenshotAs(OutputType.BYTES);
+                s.embed(screenshot, "image/png", s.getName());
+            } catch (ClassCastException cce) {
+                cce.printStackTrace();
+            }
         test.closeTestSession();
     }
 
