775a2573d21117755486fe05868915b7c4b78e06
Prashant Shukla <prashantshukla@qainfotech.com>
2020-04-27 07:01:59 +0530

    Running tests on chrome

--- src/test/java/com/qait/cucumber/RunCucumberTest.java
+++ src/test/java/com/qait/cucumber/RunCucumberTest.java
@@ -3,7 +3,8 @@ package com.qait.cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber-report/cucumber-report.html"}, 
-                features = "src/test/resources/features")
+@CucumberOptions(plugin = { "pretty",
+        "html:target/cucumber-report/cucumber-report.html",
+        "json:target/cucumber-report/cucumber-report.json" }, features = "src/test/resources/features")
 public class RunCucumberTest extends AbstractTestNGCucumberTests {
 }
